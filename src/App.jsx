8eead2a6568daf0bb19e9a6aa3c612c6d02a82b8
Bhavesh Gupta <bhaveshgupta01@gmail.com>
2026-01-18 18:26:14 -0500

    Fixed blank Screen Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Design from './pages/Design';
-import Publications from './pages/Publications';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/design" element={<Design />} />
-        <Route path="/publications" element={<Publications />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/design" element={<Design />} />
+    </Routes>
   );
 }
 
